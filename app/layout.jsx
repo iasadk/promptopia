3e640f5d65622ea0bd37f4190ce5df14cc1c132b
iasadk <89456039+iasadk@users.noreply.github.com>
2023-05-07 10:42:32 +0530

    Added Favicon to website.

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,6 +8,9 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/assets/images/logo.svg" type="image/x-icon" />
+      </head>
       <body>
         <Provider>
           <div className="main">
